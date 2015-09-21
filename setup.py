535f2f5c74efcb9668cfe8fa7c452024a7d3c016
ilovetux <me@ilovetux.com>
2015-09-21 17:02:12 -0400

    FEATURE: Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='mast.datapower.deploy',
+      version='0.6.0',
+      description='An updated deployment process automation',
+      author='Clifford Bressette IV',
+      author_email='cliffordbressette@mcindi.com',
+      maintainer="Clifford Bressette",
+      url='https://github.com/mcindi/mast.datapower.deploy',
+      namespace_packages=["mast", "mast.datapower"],
+      packages=['mast', 'mast.datapower', 'mast.datapower.deploy'],
+      license="GPL v2",
+      install_requires=[]
+     )
